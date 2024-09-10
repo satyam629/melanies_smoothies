c94b7d3ba9809a66d5f001e676c5cc19ec77c7c9
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-10 19:31:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_chosen"),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
@@ -36,7 +36,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,'is',search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
