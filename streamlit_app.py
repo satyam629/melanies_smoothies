4d88707d411f3e75b4ddfb8c9a0b9e3def7f276d
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-11 00:42:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,14 @@ from snowflake.snowpark.functions import col
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("fruit_chosen"),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
 #, col('SEARCH_ON')
 st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 #Converting the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function 
-pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 #st.stop()
 
 ingredients_list = st.multiselect("Choose up to 5 ingredients:",my_dataframe,max_selections=5)
@@ -37,7 +37,7 @@ if ingredients_list:
        for fruit_chosen in ingredients_list:
                 ingredients_string += fruit_chosen + ' '
 
-                search_on = pd_df.loc[pd_df["fruit_chosen"] == FRUIT_NAME, 'SEARCH_ON'].iloc[0]
+                search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, 'SEARCH_ON'].iloc[0]
                 st.write('The search value for ', fruit_chosen,'is',search_on, '.')
         
                 st.subheader(fruit_chosen + ' Nutrition Information')
