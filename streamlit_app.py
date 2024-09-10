e00c06406d09db0072c7769960f673a819ecb69a
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-10 10:23:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,7 @@ if time_to_insert:
         st.success(f" Woollah ! {name_on_order} You Just Ordered your Smoothie!", icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
