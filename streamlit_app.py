da329d41f5810e5417b044177d9373b2882f4262
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-11 03:02:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,10 @@ if ingredients_list:
        for fruit_chosen in ingredients_list:
                 ingredients_string += fruit_chosen + ' '
 
-                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
+                st.write('The search value for ', fruit_chosen,' is ', search_on,'.')
                 #st.dataframe(fruit_chosen)
-                st.subheader(fruit_chosen ,'SEARCH_ON' + ' Nutrition Information')
+                st.subheader(fruit_chosen ,'SEARCH_ON' + 'Nutrition Information')
                 formatted_fruit = fruit_chosen.lower()
               
                 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +  fruit_chosen)
