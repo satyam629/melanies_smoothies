8e3643243406b6734707dfb51540bea147e083cc
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-12 06:12:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,5 +62,5 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered!{name_on_order}', icon="✅")
+        st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
       
